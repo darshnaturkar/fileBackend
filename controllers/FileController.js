68645dfe52fe1aaa55d357dcf4744de4a1f8d9ff
darshnaturkar <dt1831@gmail.com>
2025-11-19 11:03:32 +0000

    filecontroller

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -5,7 +5,7 @@ exports.createFile = async (req, res) => {
         const { type, name, number, noting, support } = req.body;
         const file = new File({ type, name, number, noting, sheet: support });
         await file.save();
-        res.json({ file });
+        res.json({ msg: "File saved successfully", file });
     } catch (error) {
         res.status(500).json({ error: error });
     }
