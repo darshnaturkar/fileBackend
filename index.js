843b2c1ac2a8ab4f11226c5e7f44d40b206b7319
darshnaturkar <dt1831@gmail.com>
2025-11-19 11:51:10 +0000

    FIXED

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Static folder for uploads
 app.use('/uploads', express.static('uploads'));
