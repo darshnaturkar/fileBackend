4ddeed92e85731527a1741c3f9b00853f81d90a3
darshnaturkar <dt1831@gmail.com>
2025-11-21 06:49:57 +0000

    check

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -7,10 +7,10 @@ exports.createFile = async (req, res) => {
         console.log("Incoming body:", req.body);
         console.log("Incoming file:", req.file);
 
-        const { type, name, number, noting, sheet } = req.body; 
+        const { type, name, number, noting, sheet } = req.body;
 
-        const supportFilePath = req.file ? req.file.path : null; 
-        
+        const supportFilePath = req.file ? req.file.path : null;
+
         if (!type || !name || !number || !noting) {
             return res.status(400).json({ error: "Missing required fields or support file" });
         }
@@ -39,46 +39,53 @@ exports.createFile = async (req, res) => {
 
 
 exports.getFile = async (req, res) => {
- try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
 
-    const filter = {};
-    if (req.query.isActive) {
-      filter.isActive = req.query.isActive === "true"; // filter by active/inactive
+        const filter = {};
+        if (req.query.isActive) {
+            filter.isActive = req.query.isActive === "true"; // filter by active/inactive
+        }
+        if (req.query.name) {
+            filter.name = { $regex: req.query.name, $options: "i" }; // case-insensitive search
+        }
+
+        const total = await File.countDocuments(filter);
+        console.log(total);
+
+        const detail_check = await File.find();
+        console.log(detail_check);
+        
+
+        const details = await File.find(filter)
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            //   .populate("support")
+            //   .populate("noting")
+            .lean();
+
+        console.log(details);
+
+
+        const baseUrl = `${req.protocol}://${req.get("host")}`;
+
+        // const listDetails = details.map((detail) => ({}));
+
+        res.json({
+            details,
+            pagination: {
+                total,
+                page,
+                limit,
+                totalPages: Math.ceil(total / limit),
+            },
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-    if (req.query.name) {
-      filter.name = { $regex: req.query.name, $options: "i" }; // case-insensitive search
-    }
-
-    const total = await File.countDocuments(filter);
-console.log(total);
-
-    const details = await File.find(filter)
-      .sort({ createdAt: -1 })
-      .skip(skip)
-      .limit(limit)
-    //   .populate("support")
-    //   .populate("noting")
-      .lean();
-
-    const baseUrl = `${req.protocol}://${req.get("host")}`;
-
-    // const listDetails = details.map((detail) => ({}));
-
-    res.json({
-      details,
-      pagination: {
-        total,
-        page,
-        limit,
-        totalPages: Math.ceil(total / limit),
-      },
-    });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
 };
 
 
