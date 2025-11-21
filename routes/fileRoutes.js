37e67904c591be388b3dfa82ecb8916582cc0d8b
darshnaturkar <dt1831@gmail.com>
2025-11-21 06:14:28 +0000

    getFIle

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -7,5 +7,6 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); 
 
 router.post('/', authenticateJWT, upload.single('support'), fileController.createFile);
+router.get('/get', authenticateJWT, fileController.getFile);
 
 module.exports = router;
